adf670a4d274f8573bff0a1455529935fd8833a6
MacInnes <califormula@gmail.com>
2018-11-23 21:22:56 -0700

    mergesort working

--- sort.js
+++ sort.js
@@ -1,9 +1,30 @@
-function mergeSort(unsorted_array){
+function mergeSort(array){
+  if (array.length === 1){
+    return array;
+  }
 
+  const midpoint = Math.floor(array.length / 2);
+  const left = array.slice(0, midpoint);
+  const right = array.slice(midpoint);
+
+  return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right){
-  
+  var output = [];
+  var leftIndex = 0;
+  var rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length){
+    if (left[leftIndex] < right[rightIndex]){
+      output.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      output.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  return output.concat(left.slice(leftIndex)).concat(right.slice(rightIndex))
 }
 
 module.exports = mergeSort
